6b542e9ae73df643e94e14ab93015476f9b534de
Luiz Viana <luiz@include.io>
2015-02-24 22:07:21 -0300

    Add new resource group format and command line monitoring tool

--- reaper.spec
+++ reaper.spec
@@ -2,7 +2,7 @@
 %{!?python_sitelib: %global python_sitelib %(%{__python} -c "from distutils.sysconfig import get_python_lib; print (get_python_lib())")}
 
 Name:		reaper
-Version:	1.0
+Version:	1.1
 Release:	1%{?dist}
 Summary:	Resource controller
 
@@ -46,5 +46,10 @@ It is easy to be extended or adapted to run on other platforms.
 %{python_sitelib}/reaper/__init__.py*
 
 %changelog
+* Wed Feb 18 2015 Luiz Viana <lviana@include.io> - 1.1-1
+- Added Plesk support
+- Debian compatibility added on functions
+- Command line monitoring tool enabled
+
 * Tue Sep  9 2014 Luiz Viana <lviana@include.io> - 1.0-1
 - Initial release
--- src/lib/cgroups.py
+++ src/lib/cgroups.py
@@ -1,6 +1,8 @@
 
 import os
+import re
 import time
+import platform
 import subprocess
 import syslog
 
@@ -21,66 +23,78 @@ def _mount():
     f.close()
     syslog.syslog(syslog.LOG_INFO, 'Updated cgconfig mount entries')
 
-def _update_memory(resellers, memory):
+def _update_groups(resellers, memory, cores):
+    clist = cores.split(',');
+    core = 0
+    
     f = open(cfile, 'a')
     for reseller in resellers:
-        f.write('group group.%s {\n' % reseller)
+        f.write('group g_%s {\n' % reseller)
         f.write('\tmemory {\n')
         f.write('\t\tmemory.limit_in_bytes = %sM;\n' % memory)
         f.write('\t\tmemory.swappiness = 0;\n')
         f.write('\t}\n')
-        f.write('}\n\n')
-    f.close()
-    syslog.syslog(syslog.LOG_INFO, 'Updated cgconfig memory groups')
-
-def _update_cpuset(cores):
-    f = open(cfile, 'a')
-    for core in cores.split(','):
-        f.write('group cpu%s {\n' % core)
         f.write('\tcpuset {\n')
+        f.write('\t\tcpuset.cpus = %s;\n' % clist[core])
         f.write('\t\tcpuset.mems = 0;\n')
-        f.write('\t\tcpuset.cpus = %s;\n' % core)
         f.write('\t}\n')
         f.write('}\n\n')
+        core = core + 1
+        if core == len(clist):
+            core = 0
     f.close()
-    syslog.syslog(syslog.LOG_INFO, 'Updated cpusets on cgconfig configuration file')
-
-def cgapply():
+    syslog.syslog(syslog.LOG_INFO, 'Updated cgconfig resource groups')
+        
+def _debian_apply():
+    subprocess.call(['/usr/sbin/cgclear'])
+    time.sleep(1)
+    subprocess.call(['/usr/sbin/cgconfigparser', '-l', '/etc/cgconfig.conf'])
+    time.sleep(1)
     
-    syslog.syslog(syslog.LOG_INFO, 'Stopping cgconfig daemon')
-    subprocess.call(['/sbin/service', 'cgconfig', 'stop'])
-    syslog.syslog(syslog.LOG_INFO, 'Stopping cgred daemon')
+    try:
+        cgredpid = int(subprocess.check_output(['/bin/pidof', 'cgrulesengd']))
+        os.kill(cgredpid, 12)
+
+    except subprocess.CalledProcessError:
+        subprocess.call(['/usr/sbin/cgrulesengd', '-s'])
+
+def _redhat_apply():
     subprocess.call(['/sbin/service', 'cgred', 'stop'])
-    time.sleep(2)
-    syslog.syslog(syslog.LOG_INFO, 'Starting cgconfig daemon')
+    subprocess.call(['/sbin/service', 'cgconfig', 'stop'])
+    time.sleep(1)
     subprocess.call(['/sbin/service', 'cgconfig', 'start'])
-    time.sleep(2)
-    syslog.syslog(syslog.LOG_INFO, 'Starting cgred daemon')
+    time.sleep(1)
     subprocess.call(['/sbin/service', 'cgred', 'start'])
 
+def cgapply():
+    dist = platform.dist()[0]
+    execute = { 'debian': _debian_apply,
+                'ubuntu': _debian_apply,
+                'centos': _redhat_apply,
+                'redhat': _redhat_apply
+    }
+    execute[dist]()
+    syslog.syslog(syslog.LOG_INFO, 'Reloading cgroups hierarchy and cgrulesengd (cgred) daemon')
     
 def _create_cgrules(resellers, users, cores):
-    """ Generate cgrules
-    """
-    lcores = cores.split(',')
-    ncores = len(lcores)
-
-    counter = 0
     f = open(rfile, 'w')
-
     for reseller in resellers:
         for user in users[reseller]:
-            f.write('%s\t\tcpuset\tcpu%s\n' % (user, lcores[counter]))
-            if counter < (ncores -1):
-                counter = counter + 1
-            else:
-                counter = 0
-            f.write('%s\t\tmemory\tgroup.%s\n' % (user, reseller))
-
+            f.write('%s\t\tcpuset,memory\tg_%s\n' % (user, reseller))
     f.close()
     syslog.syslog(syslog.LOG_INFO, 'Updated cgred configuration file')
             
-
+def cgstate(target=None):
+    comment = re.compile('^#.*')
+    cgprocf = open('/proc/cgroups').read()
+    cgroups = {}
+    for line in cgprocf.splitlines():
+        if not comment.match(line):
+            cgroups[line.split()[0]] = int(line.split()[3])
+    if target is None:
+        return cgroups
+    else:
+        return {target: cgroups[target]}            
     
 def update(users, cores, memory):
     """ Update control groups rules
@@ -88,7 +102,6 @@ def update(users, cores, memory):
     resellers = users.keys()
 
     _mount()
-    _update_cpuset(cores)
-    _update_memory(users, memory)
+    _update_groups(users, memory, cores)
     _create_cgrules(resellers, users, cores)
     cgapply()
